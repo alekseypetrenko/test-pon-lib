a7c0b596b0c77c2a4a87e1e179ab903916760833
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-11-24 10:32:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <test-button />
+</template>
+
+<script setup>
+import TestButton from "./components/TestButton.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/TestButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="test-class">
+    <button>click</button>
+  </div>
+</template>
+
+<style scoped>
+.test-class {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
